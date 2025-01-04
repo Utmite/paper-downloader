46733811f3f3e407b659258fe1b7b6bc37627178
Utmite <rockimunoz94@gmail.com>
2025-01-04 15:39:34 -0300

    fix url 2

--- src/util/urls.mjs
+++ src/util/urls.mjs
@@ -1,5 +1,5 @@
 
 export default {
-    "VersionsAvailablePaper": "https://api.papermc.io/api/v2/projects/paper",
-    "ProjectsAvailable": "https://api.papermc.io/api/v2/projects/"
+    "VersionsAvailablePaper": "https://api.papermc.io/v2/projects/paper",
+    "ProjectsAvailable": "https://api.papermc.io/v2/projects/"
 };
